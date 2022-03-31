b301a4e2dff6518eba533253ccf901282af187ca
General Zelgius <mattlow123@gmail.com>
2022-03-31 18:51:34 -0400

    successfully implemented helpers in handlebars

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const helpers = require('./utils/helpers');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
